562597a36158a172f9832a018e45a7060cd8bfce
Luiz Andre Zorzi Markesz <andre.z.markesz@gmail.com>
2021-05-03 16:38:30 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <div className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/andrezeeem" target="_blank" rel="noreferrer">Luiz André Zorzi Markesz</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                Evento promovido pela escola Devsuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
