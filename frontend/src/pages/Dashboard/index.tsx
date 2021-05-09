4ae7a4cb4bb20a8ccbbd87a65ce37bb4739d2e05
Luiz Andre Zorzi Markesz <andre.z.markesz@gmail.com>
2021-05-09 00:37:42 -0300

    Presentation adjustment

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -12,18 +12,18 @@ const Dashboard = () => {
         <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
         <div className="row px-3">
-          <div className="col sm-6">
+          <div className="col-sm-6">
             <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
             <BarChart />
           </div>
-          <div className="col sm-6">
-            <h5 className="text-center text secondary">Todas Vendas</h5>
+          <div className="col-sm-6">
+            <h5 className="text-center text secondary">Todas as Vendas</h5>
             <DonutChart />
           </div>
         </div>
 
         <div className="py-3">
-          <h2 className="text-primary">Todas Vendas</h2>
+          <h2 className="text-primary">Todas as Vendas</h2>
 
         </div>
 
